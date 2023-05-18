37f3ebfe619877bf602e929d4db1d9698db52d8f
Md Tarequl Islam <mdtarequlislam634@gmail.com>
2023-05-18 23:54:13 +0600

    init commit

---
+++ role-base-authentication/src/test/java/com/tareq23/rolebaseauth/RoleBaseAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tareq23.rolebaseauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBaseAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
