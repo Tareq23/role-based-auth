9c9d0dd16505be6a331d80066c11daeba65475c4
Md Tarequl Islam <mdtarequlislam634@gmail.com>
2023-05-22 13:24:11 +0600

    without deprecated web security configurer adapter

--- role-base-authentication/src/main/java/com/tareq23/rolebaseauth/SecurityConfigure.java
+++ role-base-authentication/src/main/java/com/tareq23/rolebaseauth/SecurityConfigure.java
@@ -1,24 +1,45 @@
 package com.tareq23.rolebaseauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
-@EnableWebSecurity
-public class SecurityConfigure extends WebSecurityConfigurerAdapter{
+//@EnableWebSecurity
+//public class SecurityConfigure extends WebSecurityConfigurerAdapter{
+//
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication()
+//			.withUser("tareq23")
+//			.password("tareq23")
+//			.roles("admin")
+//			.and()
+//			.withUser("tareq")
+//			.password("tareq")
+//			.roles("user");
+//	}
+//	
+//	@Bean
+//	public NoOpPasswordEncoder passwordEncoder() {
+//		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+//	}
+//	
+//
+//}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("tareq23")
-			.password("tareq23")
-			.roles("admin")
-			.and()
-			.withUser("tareq")
-			.password("tareq")
-			.roles("user");
+
+@Configuration
+public class SecurityConfigure{
+	
+	
+	@Autowired
+	void registerProvider(AuthenticationManagerBuilder auth) throws Exception {
+	    auth.inMemoryAuthentication().withUser("tareq23").password("tareq23").roles("user");
 	}
 	
 	@Bean
@@ -26,7 +47,4 @@ public class SecurityConfigure extends WebSecurityConfigurerAdapter{
 		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
 	}
 	
-	
-	
-
 }
